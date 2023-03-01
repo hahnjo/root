llvm/llvm-project

[JITLink][RISCV] Ignore R_RISCV_RELAX and check R_RISCV_ALIGN
It is fine to not implement and ignore linker relaxation for now, but
we need to check the alignment. Luckily, an alignment of only 2 bytes
is the most common case when interpreting C++ code in clang-repl, and
already guaranteed by the length of compressed instructions.

Differential Revision: https://reviews.llvm.org/D129159

llvm/lib/ExecutionEngine/JITLink/ELF_riscv.cpp
@@ -494,6 +494,30 @@ private:
                             Block &BlockToFix) {
     using Base = ELFLinkGraphBuilder<ELFT>;
 
+    uint32_t Type = Rel.getType(false);
+    // We do not implement linker relaxation, except what is required for
+    // alignment (see below).
+    if (Type == llvm::ELF::R_RISCV_RELAX)
+      return Error::success();
+
+    int64_t Addend = Rel.r_addend;
+    if (Type == llvm::ELF::R_RISCV_ALIGN) {
+      uint64_t Alignment = PowerOf2Ceil(Addend);
+      // FIXME: Implement support for ensuring alignment together with linker
+      // relaxation; 2 bytes are guaranteed by the length of compressed
+      // instructions, so this does not need any action from our side.
+      if (Alignment > 2)
+        return make_error<JITLinkError>(
+            formatv("Unsupported relocation R_RISCV_ALIGN with alignment {0} "
+                    "larger than 2 (addend: {1})",
+                    Alignment, Addend));
+      return Error::success();
+    }
+
+    Expected<riscv::EdgeKind_riscv> Kind = getRelocationKind(Type);
+    if (!Kind)
+      return Kind.takeError();
+
     uint32_t SymbolIndex = Rel.getSymbol(false);
     auto ObjSymbol = Base::Obj.getRelocationSymbol(Rel, Base::SymTabSec);
     if (!ObjSymbol)
@@ -508,12 +532,6 @@ private:
                   Base::GraphSymbols.size()),
           inconvertibleErrorCode());
 
-    uint32_t Type = Rel.getType(false);
-    Expected<riscv::EdgeKind_riscv> Kind = getRelocationKind(Type);
-    if (!Kind)
-      return Kind.takeError();
-
-    int64_t Addend = Rel.r_addend;
     auto FixupAddress = orc::ExecutorAddr(FixupSect.sh_addr) + Rel.r_offset;
     Edge::OffsetT Offset = FixupAddress - BlockToFix.getAddress();
     Edge GE(*Kind, Offset, *GraphSymbol, Addend);

hahnjo/root

interpreter/llvm/src/lib/ExecutionEngine/JITLink/ELF_riscv.cpp
@@ -469,6 +469,25 @@ private:
                  << "Name: " << Base::Obj.getRelocationTypeName(Type) << "\n";
         });
 
+        // We do not implement linker relaxation, except what is required for
+        // alignment (see below).
+        if (Type == llvm::ELF::R_RISCV_RELAX)
+          continue;
+
+        int64_t Addend = Rela.r_addend;
+        if (Type == llvm::ELF::R_RISCV_ALIGN) {
+          uint64_t Alignment = PowerOf2Ceil(Addend);
+          // FIXME: Implement support for ensuring alignment together with linker
+          // relaxation; 2 bytes are guaranteed by the length of compressed
+          // instructions, so this does not need any action from our side.
+          if (Alignment > 2)
+            return make_error<JITLinkError>(
+                formatv("Unsupported relocation R_RISCV_ALIGN with alignment {0} "
+                        "larger than 2 (addend: {1})",
+                        Alignment, Addend));
+          continue;
+        }
+
         auto SymbolIndex = Rela.getSymbol(false);
         auto Symbol = Base::Obj.getRelocationSymbol(Rela, Base::SymTabSec);
         if (!Symbol)
@@ -486,7 +505,6 @@ private:
                   std::to_string(Base::GraphSymbols.size()),
               llvm::inconvertibleErrorCode());
         }
-        int64_t Addend = Rela.r_addend;
         JITTargetAddress FixupAddress =
             (*UpdateSection)->sh_addr + Rela.r_offset;
 